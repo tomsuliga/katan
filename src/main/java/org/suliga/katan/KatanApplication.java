4bf2bc286aea9b594f1cc5d972e2498c5994ec4a
tom <tom2wheeler@gmail.com>
2017-10-30 20:16:04 -0700

    Initial checkin of default files

---
+++ src/main/java/org/suliga/katan/KatanApplication.java
@@ -0,0 +1,12 @@
+package org.suliga.katan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatanApplication.class, args);
+	}
+}
---
+++ src/test/java/org/suliga/katan/KatanApplicationTests.java
@@ -0,0 +1,16 @@
+package org.suliga.katan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KatanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
